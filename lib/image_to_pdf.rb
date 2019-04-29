32d81eaf98c5cdf0bff95c5648ed4307efabe4d7
Yuya.Nishida <yuya@j96.org>
2019-04-29 22:55:24 +0900

    chore: sort on loading

--- lib/image_to_pdf.rb
+++ lib/image_to_pdf.rb
@@ -3,7 +3,7 @@ module ImageToPdf
 
   # set autoload
   lib_path = Pathname(__dir__)
-  Pathname.glob(lib_path / "image_to_pdf/*.rb") do |path|
+  Pathname.glob(lib_path / "image_to_pdf/*.rb").sort.each do |path|
     no_ext_path = path.relative_path_from(lib_path).sub_ext("")
     class_name = no_ext_path.basename.to_s.capitalize.to_sym
     next if class_name == :Version
