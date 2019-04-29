8f26c124d103032992fd2dd096a2b4ae10db6bc3
Yuya.Nishida <yuya@j96.org>
2019-04-29 22:57:53 +0900

    chore: support multi-word class loading

--- lib/image_to_pdf.rb
+++ lib/image_to_pdf.rb
@@ -5,7 +5,8 @@ module ImageToPdf
   lib_path = Pathname(__dir__)
   Pathname.glob(lib_path / "image_to_pdf/*.rb").sort.each do |path|
     no_ext_path = path.relative_path_from(lib_path).sub_ext("")
-    class_name = no_ext_path.basename.to_s.capitalize.to_sym
+    class_name = no_ext_path.basename.to_s.
+                   split("_").map(&:capitalize).join.to_sym
     next if class_name == :Version
     autoload(class_name, no_ext_path.to_s)
   end
