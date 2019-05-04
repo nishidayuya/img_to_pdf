2757e724897caad45d573d8f11eac3fa624eb883
Yuya.Nishida <yuya@j96.org>
2019-05-04 22:30:24 +0900

    feat: add ImageToPdf::PaperSizeParser

---
+++ lib/image_to_pdf/paper_size_parser.rb
@@ -0,0 +1,26 @@
+require "image_to_pdf"
+
+module ImageToPdf::PaperSizeParser
+  extend self
+
+  AVAILABLE_DIRECTIONS = %i[landscape portrait]
+
+  # @param [String] s paper size text. "a4-landscape", "b3-portrait", etc.
+  # @return [ImageToPdf::Dimension] parsed paper dimension. points.
+  def call(s)
+    paper_size, direction = *s.split("-")
+    direction = direction.downcase.to_sym
+    if !AVAILABLE_DIRECTIONS.include?(direction)
+      raise ImageToPdf::ParserError, "invalid paper direction: #{s.inspect}"
+    end
+    raw_size_pt = PDF::Core::PageGeometry::SIZES[paper_size.upcase]
+    if !raw_size_pt
+      raise ImageToPdf::ParserError, "invalid paper size: #{s.inspect}"
+    end
+
+    paper_dimension_pt = ImageToPdf::Dimension.from_array(raw_size_pt)
+    paper_dimension_pt = paper_dimension_pt.justify_direction(direction)
+
+    return paper_dimension_pt
+  end
+end
---
+++ test/unit/paper_size_parser_test.rb
@@ -0,0 +1,31 @@
+require "test_helper"
+
+require "pdf/core/page_geometry"
+
+class ImageToPdf::PaperSizeParserTest < TestCase
+  data {
+    {
+      "a4-landscape" => PDF::Core::PageGeometry::SIZES["A4"].reverse,
+      "b3-portrait" => PDF::Core::PageGeometry::SIZES["B3"],
+    }.each.with_object({}) do |(paper_size_text, expected_size_ary), h|
+      h[paper_size_text] = {
+        paper_size_text: paper_size_text,
+        expected: ImageToPdf::Dimension.from_array(expected_size_ary),
+      }
+    end
+  }
+  test("parsable") do |paper_size_text:, expected:|
+    assert_equal(expected, ImageToPdf::PaperSizeParser.(paper_size_text))
+  end
+
+  data {
+    %w[x5-landscape a4-foobar].each.with_object({}) do |s, h|
+      h[s] = s
+    end
+  }
+  test("not parsable") do |paper_size_text|
+    assert_raise(ImageToPdf::ParserError) do
+      ImageToPdf::PaperSizeParser.(paper_size_text)
+    end
+  end
+end
