ba6c56a3d15b6dfd7e4664d93304d46a7f898c15
Yuya.Nishida <yuya@j96.org>
2019-05-06 22:49:06 +0900

    feat: use ImageToPdf::CliOption

--- lib/image_to_pdf/cli.rb
+++ lib/image_to_pdf/cli.rb
@@ -7,18 +7,18 @@ module ImageToPdf::Cli
   extend self
 
   def run(argv)
-    input_path, output_path = *parse_options(argv).map { |path| Pathname(path) }
-    page_dimension_pt = ImageToPdf::PaperSizeParser.(@paper_size)
+    option = ImageToPdf::CliOption.from_argv(argv)
+    page_dimension_pt = ImageToPdf::PaperSizeParser.(option.paper_size_text)
 
     canvas_dimension_pt = ImageToPdf::Dimension.new(
-      width: ((page_dimension_pt.width - @left_margin_pt - @right_margin_pt) *
-              @n_horizontal_pages),
-      height: ((page_dimension_pt.height - @top_margin_pt - @top_margin_pt) *
-               @n_vertical_pages),
+      width: ((page_dimension_pt.width - option.margin_pt.left - option.margin_pt.right) *
+              option.n_horizontal_pages),
+      height: ((page_dimension_pt.height - option.margin_pt.top - option.margin_pt.bottom) *
+               option.n_vertical_pages),
     )
     canvas_dimension_in = canvas_dimension_pt.pt_to_in
 
-    image_dimension_px = parse_image_dimension(input_path)
+    image_dimension_px = parse_image_dimension(option.input_path)
     dpi_from_width = image_dimension_px.width / canvas_dimension_in.width
     dpi_from_height = image_dimension_px.height / canvas_dimension_in.height
     canvas_dpi = [dpi_from_width, dpi_from_height].max
@@ -26,26 +26,23 @@ module ImageToPdf::Cli
 
     tmp_path = Pathname(Dir.mktmpdir)
     tmp_image_dimension_px = ImageToPdf::Dimension.new(
-      width: image_dimension_px.width / @n_horizontal_pages,
-      height: image_dimension_px.height / @n_vertical_pages,
+      width: image_dimension_px.width / option.n_horizontal_pages,
+      height: image_dimension_px.height / option.n_vertical_pages,
     )
 
     pdf = Prawn::Document.new(
       page_size: page_dimension_pt.to_a,
-      left_margin: @left_margin_pt,
-      right_margin: @right_margin_pt,
-      top_margin: @top_margin_pt,
-      bottom_margin: @bottom_margin_pt,
+      margin: option.margin_pt.to_a,
       skip_page_creation: true,
       info: {
         Creator: "image_to_pdf version #{ImageToPdf::VERSION}",
       }
     )
-    @n_vertical_pages.times do |i_y|
-      @n_horizontal_pages.times do |i_x|
+    option.n_vertical_pages.times do |i_y|
+      option.n_horizontal_pages.times do |i_x|
         tmp_image_path = tmp_path / "#{i_x}-#{i_y}"
 
-        image = MiniMagick::Image.open(input_path)
+        image = MiniMagick::Image.open(option.input_path)
         image.crop("#{tmp_image_dimension_px.width}x#{tmp_image_dimension_px.height}+#{tmp_image_dimension_px.width * i_x}+#{tmp_image_dimension_px.height * i_y}")
         image.write(tmp_image_path)
 
@@ -53,13 +50,13 @@ module ImageToPdf::Cli
         pdf.image(
           tmp_image_path,
           scale: canvas_scale,
-          at: [0, canvas_dimension_pt.height / @n_vertical_pages],
+          at: [0, canvas_dimension_pt.height / option.n_vertical_pages],
         )
       end
     end
-    pdf.render_file(output_path)
+    pdf.render_file(option.output_path)
   rescue ImageToPdf::Error => e
-    raise if @debug
+    raise if option.debug
     STDERR.puts("#{e.class.name}: #{e.message}")
     exit(1)
   ensure
@@ -67,50 +64,6 @@ module ImageToPdf::Cli
   end
 
   private
-
-  BANNER = <<EOS.chomp
-Usage: #{File.basename(Process.argv0)} [options] input_image_path output_pdf_path
-EOS
-
-  # @param [Array<String>] argv program arguments.
-  # @return [Array<(String, String)>] `[input image path, output pdf path]`.
-  def parse_options(argv)
-    @debug = false
-    @paper_size = "a4-landscape"
-    @n_vertical_pages = 1
-    @n_horizontal_pages = 1
-    @left_margin_pt = @right_margin_pt = @top_margin_pt = @bottom_margin_pt =
-      ImageToPdf::Unit.convert_mm_to_pt(10)
-
-    parser = OptionParser.new
-    parser.version = ImageToPdf::VERSION
-    parser.banner = BANNER
-    parser.separator("")
-    parser.separator("Options:")
-    parser.summary_indent = ""
-    parser.on("--paper-size=SIZE",
-              "specify paper size. 'a4-landscape'(default), 'b3-portrait', etc.") do |v|
-      @paper_size = v
-    end
-    parser.on("--vertical-pages=INTEGER",
-              "specify number of vertical pages. default 1.") do |v|
-      @n_vertical_pages = ImageToPdf::IntegerParser.(v)
-    end
-    parser.on("--horizontal-pages=INTEGER",
-              "specify number of horizontal pages. default 1.") do |v|
-      @n_horizontal_pages = ImageToPdf::IntegerParser.(v)
-    end
-    parser.on("--debug") do
-      @debug = true
-    end
-
-    input_path, output_path = *parser.parse(argv)
-    if !output_path
-      puts(parser.help)
-      exit(1)
-    end
-    return input_path, output_path
-  end
 
   # @param [Pathname] path path to image file.
   # @return [ImageToPdf::Dimension] image dimension. pixels.
