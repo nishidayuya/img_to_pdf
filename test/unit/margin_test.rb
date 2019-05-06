d39b24b64910bfbb6700f1d7689b4d4aea4f1230
Yuya.Nishida <yuya@j96.org>
2019-05-06 22:46:01 +0900

    test: specify ImageToPdf::Margin#to_a

---
+++ test/unit/margin_test.rb
@@ -0,0 +1,12 @@
+require "test_helper"
+
+class ImageToPdf::MarginTest < TestCase
+  sub_test_case("#to_a") do
+    test("return array") do
+      assert_equal(
+        [10, 20, 30, 40],
+        ImageToPdf::Margin.new(top: 10, right: 20, bottom: 30, left: 40).to_a,
+      )
+    end
+  end
+end
