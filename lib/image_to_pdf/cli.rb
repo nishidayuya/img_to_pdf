2766391f179f8e3593e1e07da76b1598f9a3b649
Yuya.Nishida <yuya@j96.org>
2019-05-24 23:00:28 +0900

    refactor: use ImageToPdf::Image

--- lib/image_to_pdf/cli.rb
+++ lib/image_to_pdf/cli.rb
@@ -20,16 +20,15 @@ module ImageToPdf::Cli
     )
     canvas_dimension_in = canvas_dimension_pt.pt_to_in
 
-    image_dimension_px = parse_image_dimension(option.input_path)
-    dpi_from_width = image_dimension_px.width / canvas_dimension_in.width
-    dpi_from_height = image_dimension_px.height / canvas_dimension_in.height
+    image = ImageToPdf::Image.from_path(option.input_path)
+    dpi_from_width = image.dimension_px.width / canvas_dimension_in.width
+    dpi_from_height = image.dimension_px.height / canvas_dimension_in.height
     canvas_dpi = [dpi_from_width, dpi_from_height].max
     canvas_scale = 72 / canvas_dpi
 
-    tmp_path = Pathname(Dir.mktmpdir)
     tmp_image_dimension_px = ImageToPdf::Dimension.new(
-      width: image_dimension_px.width / option.n_horizontal_pages,
-      height: image_dimension_px.height / option.n_vertical_pages,
+      width: image.dimension_px.width / option.n_horizontal_pages,
+      height: image.dimension_px.height / option.n_vertical_pages,
     )
 
     pdf = Prawn::Document.new(
@@ -42,18 +41,17 @@ module ImageToPdf::Cli
     )
     option.n_vertical_pages.times do |i_y|
       option.n_horizontal_pages.times do |i_x|
-        tmp_image_path = tmp_path / "#{i_x}-#{i_y}"
-
-        image = MiniMagick::Image.open(option.input_path)
-        image.crop("#{tmp_image_dimension_px.width}x#{tmp_image_dimension_px.height}+#{tmp_image_dimension_px.width * i_x}+#{tmp_image_dimension_px.height * i_y}")
-        image.write(tmp_image_path)
-
-        pdf.start_new_page
-        pdf.image(
-          tmp_image_path,
-          scale: canvas_scale,
-          at: [0, canvas_dimension_pt.height / option.n_vertical_pages],
-        )
+        image.crop(tmp_image_dimension_px.width * i_x,
+                   tmp_image_dimension_px.height * i_y,
+                   tmp_image_dimension_px.width,
+                   tmp_image_dimension_px.height) do |sub_image|
+          pdf.start_new_page
+          pdf.image(
+            sub_image.path,
+            scale: canvas_scale,
+            at: [0, canvas_dimension_pt.height / option.n_vertical_pages],
+          )
+        end
       end
     end
     pdf.render_file(option.output_path)
@@ -61,20 +59,5 @@ module ImageToPdf::Cli
     raise if option.debug
     STDERR.puts("#{e.class.name}: #{e.message}")
     exit(1)
-  ensure
-    tmp_path&.rmtree
-  end
-
-  private
-
-  # @param [Pathname] path path to image file.
-  # @return [ImageToPdf::Dimension] image dimension. pixels.
-  def parse_image_dimension(path)
-    image_klass = Prawn::Images.const_get(path.extname.upcase.sub(/\A\./, ""))
-    image = image_klass.new(path.read)
-    return ImageToPdf::Dimension.new(
-      width: image.width,
-      height: image.height,
-    )
   end
 end
