9b4597f27dfad0c9e63fb743a4e2d67afa6de412
Yuya.Nishida <yuya@j96.org>
2019-05-25 22:20:52 +0900

    feat: add ImageToPdf::FitPageDocument

---
+++ lib/image_to_pdf/fit_page_document.rb
@@ -0,0 +1,92 @@
+require "prawn"
+
+require "image_to_pdf"
+
+class ImageToPdf::FitPageDocument
+  # @param [ImageToPdf::Dimension] page_dimension_pt page size. points.
+  # @param [ImageToPdf::Margin] margin_pt margin size. points.
+  # @param [ImageToPdf::Image] image source image.
+  # @param [Integer] n_horizontal_pages number of horizontal pages.
+  # @param [Integer] n_vertical_pages number of vertical pages.
+  # @return [ImageToPdf::FitPageDocument] document to render PDF.
+  def self.create(page_dimension_pt:, margin_pt:, image:,
+                  n_horizontal_pages: 1, n_vertical_pages: 1)
+    result = new(page_dimension_pt: page_dimension_pt, margin_pt: margin_pt,
+                 n_horizontal_pages: n_horizontal_pages,
+                 n_vertical_pages: n_vertical_pages,
+                 image: image)
+    result.draw
+    return result
+  end
+
+  def initialize(page_dimension_pt:, margin_pt:, image:,
+                 n_horizontal_pages:, n_vertical_pages:)
+    @page_dimension_pt = page_dimension_pt
+    @margin_pt = margin_pt
+    @image = image
+    @n_horizontal_pages = n_horizontal_pages
+    @n_vertical_pages = n_vertical_pages
+
+    @pdf = Prawn::Document.new(
+      page_size: @page_dimension_pt.to_a,
+      margin: @margin_pt.to_a,
+      skip_page_creation: true,
+      info: {
+        Creator: "image_to_pdf version #{ImageToPdf::VERSION}",
+      },
+    )
+  end
+
+  def draw
+    canvas_dimension_pt = determine_canvas_dimension_pt
+    canvas_scale = determine_canvas_scale(canvas_dimension_pt)
+    sub_image_dimension_px = ImageToPdf::Dimension.new(
+      width: @image.dimension_px.width / @n_horizontal_pages,
+      height: @image.dimension_px.height / @n_vertical_pages,
+    )
+    each_sub_image(sub_image_dimension_px) do |sub_image|
+      @pdf.start_new_page
+      @pdf.image(
+        sub_image.path,
+        scale: canvas_scale,
+        at: [0, canvas_dimension_pt.height / @n_vertical_pages],
+      )
+    end
+  end
+
+  # @param [Pathname] output_path path to PDF output.
+  def render_file(output_path)
+    @pdf.render_file(output_path)
+  end
+
+  private
+
+  def determine_canvas_dimension_pt
+    w = (@page_dimension_pt.width - @margin_pt.left - @margin_pt.right) *
+        @n_horizontal_pages
+    h = (@page_dimension_pt.height - @margin_pt.top - @margin_pt.bottom) *
+        @n_vertical_pages
+    return ImageToPdf::Dimension.new(width: w, height: h)
+  end
+
+  def determine_canvas_scale(canvas_dimension_pt)
+    canvas_dimension_in = canvas_dimension_pt.pt_to_in
+    dpi_from_width = @image.dimension_px.width / canvas_dimension_in.width
+    dpi_from_height = @image.dimension_px.height / canvas_dimension_in.height
+    canvas_dpi = [dpi_from_width, dpi_from_height].max
+    return 72 / canvas_dpi
+  end
+
+  def each_sub_image(sub_image_dimension_px)
+    @n_vertical_pages.times do |i_y|
+      @n_horizontal_pages.times do |i_x|
+        @image.crop(sub_image_dimension_px.width * i_x,
+                    sub_image_dimension_px.height * i_y,
+                    sub_image_dimension_px.width,
+                    sub_image_dimension_px.height) do |sub_image|
+          yield(sub_image)
+        end
+      end
+    end
+  end
+end
---
+++ test/unit/fit_page_document_test.rb
@@ -0,0 +1,43 @@
+require "test_helper"
+
+class ImageToPdf::FitPageDocumentTest < TestCase
+  sub_test_case(".render_file") do
+    setup_tmp_path(:@output_path)
+
+    data {
+      margin_length_pt = ImageToPdf::Unit.convert_mm_to_pt(10)
+      margin_pt = ImageToPdf::Margin.new(
+        top: margin_length_pt,
+        right: margin_length_pt,
+        bottom: margin_length_pt,
+        left: margin_length_pt,
+      )
+      image = ImageToPdf::Image.from_path(TEST_ASSETS_PATH / "i.png")
+      [
+        [
+          "i-default.pdf",
+          "a4-landscape",
+          {},
+        ],
+        [
+          "i-b5-portrait-3x4.pdf",
+          "b5-portrait",
+          {n_horizontal_pages: 3, n_vertical_pages: 4},
+        ]
+      ].each.with_object({}) do |(expected_filename, page_size_text, args), h|
+        h[expected_filename] = {
+          args: args.merge(
+            page_dimension_pt: ImageToPdf::PaperSizeParser.(page_size_text),
+            margin_pt: margin_pt,
+            image: image,
+          ),
+          expected_path: TEST_ASSETS_PATH / expected_filename,
+        }
+      end
+    }
+    test("render PDF file") do |args:, expected_path:|
+      ImageToPdf::FitPageDocument.create(**args).render_file(@output_path)
+      assert_file_content(expected_path, @output_path)
+    end
+  end
+end
