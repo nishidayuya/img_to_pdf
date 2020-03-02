6e57f179aa222929bb07f132d5efe948411c54a2
Yuya.Nishida <yuya@j96.org>
2020-03-02 22:48:40 +0900

    test: add lcov format coverage result

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,4 +1,13 @@
 require "simplecov"
+require "simplecov-lcov"
+SimpleCov::Formatter::LcovFormatter.config do |c|
+  c.report_with_single_file = true
+  c.single_report_path = "coverage/lcov.info"
+end
+SimpleCov.formatters = [
+  SimpleCov::Formatter::HTMLFormatter,
+  SimpleCov::Formatter::LcovFormatter,
+]
 SimpleCov.start
 
 require "digest/md5"
