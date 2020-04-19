aa9aa8a7ea1c8e99304ac89c6fe5a6b21767060a
Yuya.Nishida <yuya@j96.org>
2020-04-19 22:00:51 +0900

    style: to one-line guard clause (Style/IfUnlessModifier)

--- lib/img_to_pdf/paper_size_parser.rb
+++ lib/img_to_pdf/paper_size_parser.rb
@@ -13,13 +13,10 @@ module ImgToPdf::PaperSizeParser
   def call(s)
     paper_size, direction = *s.split("-")
     direction = direction.downcase.to_sym
-    if !AVAILABLE_DIRECTIONS.include?(direction)
-      raise ImgToPdf::ParserError, "invalid paper direction: #{s.inspect}"
-    end
+    raise ImgToPdf::ParserError, "invalid paper direction: #{s.inspect}" if !AVAILABLE_DIRECTIONS.include?(direction)
+
     raw_size_pt = PDF::Core::PageGeometry::SIZES[paper_size.upcase]
-    if !raw_size_pt
-      raise ImgToPdf::ParserError, "invalid paper size: #{s.inspect}"
-    end
+    raise ImgToPdf::ParserError, "invalid paper size: #{s.inspect}" if !raw_size_pt
 
     paper_dimension_pt = ImgToPdf::Dimension.from_array(raw_size_pt)
     paper_dimension_pt = paper_dimension_pt.justify_direction(direction)
