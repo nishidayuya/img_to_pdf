6a9ec9c7d329845ea8b95a1ce0e5f01960925cb0
Yuya.Nishida <yuya@j96.org>
2021-07-31 15:52:07 +0900

    test: add UPDATE_EXPECTED_PDF_IF_SAME_IMAGES environment test helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -33,11 +33,51 @@ class TestCase < Test::Unit::TestCase
   private
 
   def assert_pdf_content(expected_path, actual_path)
+    expected_digest = pdf_content_digest(expected_path)
+    actual_digest = pdf_content_digest(actual_path)
+
+    if !ENV["UPDATE_EXPECTED_PDF_IF_SAME_IMAGES"].to_s.empty? &&
+       expected_digest != actual_digest &&
+       update_expected_if_same_images(expected_path, actual_path)
+      expected_digest = pdf_content_digest(expected_path)
+      actual_digest = pdf_content_digest(actual_path)
+    end
+
     assert_equal(
-      pdf_content_digest(expected_path),
-      pdf_content_digest(actual_path),
+      expected_digest,
+      actual_digest,
       "#{expected_path} != #{actual_path}",
     )
+  end
+
+  # need following commands:
+  #
+  # - pdftocairo: apt install -y poppler-utils
+  # - pixelmatch: npm install -g pixelmatch
+  def update_expected_if_same_images(expected_path, actual_path)
+    Dir.mktmpdir do |d|
+      tmp_root = Pathname(d)
+      expected_paths = create_png_images_from_pdf(expected_path, tmp_root, "expected")
+      actual_paths = create_png_images_from_pdf(actual_path, tmp_root, "actual")
+      if expected_paths.length != actual_paths.length
+        puts("number of paths are different: expected=#{expected_paths.length} actual=#{actual_paths.length}")
+        return false
+      end
+      expected_paths.zip(actual_paths).each do |expected, actual|
+        if !system("pixelmatch #{expected} #{actual}")
+          puts("different page is detected: expected=#{expected} actual=#{actual}")
+          return false
+        end
+      end
+    end
+    FileUtils.cp(actual_path, expected_path)
+    return true
+  end
+
+  def create_png_images_from_pdf(pdf_path, output_root, filename_prefix)
+    system(*%W[pdftocairo -png #{pdf_path} #{output_root}/#{filename_prefix}], exception: true)
+    paths = Pathname.glob(output_root / "#{filename_prefix}-*.png").sort
+    return paths
   end
 
   def pdf_content_digest(pdf_path)
